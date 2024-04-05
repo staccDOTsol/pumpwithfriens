5d2bccdded0dc09bb66c304d9b17cdb49ad13b96
Your Name <you@example.com>
2024-04-05 00:36:46 +0000

    ic2

--- programs/pumpwithfriens/src/lib.rs
+++ programs/pumpwithfriens/src/lib.rs
@@ -1,11 +1,27 @@
 use anchor_lang::prelude::*;
 use std::str::FromStr;
-use anchor_lang::{solana_program::{instruction::Instruction, program::invoke_signed}, system_program::{self, Transfer}};
+use anchor_lang::{solana_program::instruction::Instruction, system_program::{self, Transfer}};
 
 declare_id!("22Jwcjoi8ffW8gG7LYqVX5mHK4u7RyN5xNf8TE1hsS9P");
 
+fn lamports_per_token(remaining_percentage: f64) -> f64 {
+    let a: f64 = 13169.68; // Estimated parameter 'a' of the power law curve
+    let b: f64 = 1.705;    // Estimated parameter 'b' of the power law curve
+    let lamports_per_sol: f64 = 1_000_000_000.0; // 1 SOL = 1,000,000,000 lamports
+
+    // Calculate tokens per SOL using the bonding curve
+    // should this be decimal or percentage?
+    let tokens_per_sol = a * remaining_percentage.powf(b);
+
+    // Invert the relationship to get lamports per token
+    lamports_per_sol / tokens_per_sol
+}
+
 #[program]
 pub mod pumpinator {
+
+    use anchor_lang::solana_program::{program::invoke, program_pack::Pack};
+    use anchor_spl::token::spl_token::state::Account;
 
     use super::*;
 
@@ -24,35 +40,101 @@ pub mod pumpinator {
     }
     pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
         let bump = &[ctx.bumps.friend];
-        let signers_seeds: &[&[&[u8]]] = &[&[b"friend", ctx.accounts.authority.key.as_ref()], &[bump]];
-        let cpi_context = CpiContext::new_with_signer(ctx.accounts.system_program.to_account_info().clone(), Transfer {
+
+        let authority_seeds = &[
+            b"friend",
+            ctx.accounts.authority.key.as_ref(),
+            bump
+        ];
+        let signers = &[&authority_seeds[..]];
+                let cpi_context = CpiContext::new_with_signer(ctx.accounts.system_program.to_account_info().clone(), Transfer {
             from: ctx.accounts.friend.to_account_info(),
             to: ctx.accounts.authority.to_account_info(),
-        }, signers_seeds);
+        }, signers);
         system_program::transfer(cpi_context, amount)?;
         Ok(())
     }
-    pub fn pump(ctx: Context<Pump>, data: Vec<u8>) -> Result<()> {
+    
+    
+    pub fn pump<'info>(ctx: Context<'_, '_, '_, 'info, Pump<'info>>) -> Result<()> {
         let remaining_accounts = ctx.remaining_accounts;
-        let mut keys = remaining_accounts.iter().map(|account| account.to_account_info()).collect::<Vec<_>>();
-        assert!(keys[6].key == ctx.accounts.friend.to_account_info().key);
-        let data = data.to_vec();
-        let bump = &[ctx.bumps.friend];
+        let curve_ata = remaining_accounts[4].to_account_info();
+        let decoded_ata: Account = Account::unpack(&curve_ata.data.borrow())?;
+        let total_supply = 1_000_000_000f64; // Total supply is a billion
+        let remaining_amount = decoded_ata.amount as f64 / 1_000_000_f64; // Remaining amount
 
-        let signers_seeds: &[&[&[u8]]] = &[&[b"friend", ctx.accounts.authority.key.as_ref()], &[bump]];
+        // Calculate remaining percentage
+        let remaining_percentage = (remaining_amount / total_supply) * 100f64; // Convert to percentage
+
+        let lamports_per_token = lamports_per_token(remaining_percentage); //1300
+
+        let data = hex::decode("66063d1201daebea972771084101000028e3600c00000000").unwrap();
+        let amount = u64::from_le_bytes(data[8..16].try_into().unwrap()); // 13000000
+        let lamports_per_amount = (lamports_per_token * amount as f64) as u64 / 1_000_000; // 13000000 * 1300 = 16900000
+
+        let mut account_metas = ctx.remaining_accounts.to_vec().iter().map(|account| AccountMeta {
+            pubkey: *account.key,
+            is_signer: account.is_signer,
+            is_writable: account.is_writable,
+        }).collect::<Vec<AccountMeta>>();
+        // transfer all lamports to acocunt_metas[6] from freind
+        account_metas[6].is_signer = true;
 
         let instruction = Instruction {
             program_id: Pubkey::from_str("6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P").unwrap(),
-            accounts: keys.iter().map(|account| AccountMeta {
-                pubkey: *account.key,
-                is_signer: account.is_signer,
-                is_writable: account.is_writable,
-            }).collect::<Vec<_>>(),
-            data
+            accounts: account_metas,
+            data: data.clone()
         };
-        invoke_signed(&instruction, &remaining_accounts, signers_seeds)?;
+        let from_account =  &mut ctx.accounts.friend.to_account_info();
+        let to_account = &mut ctx.accounts.jare.to_account_info();
+        msg!("lamports_per_amount: {}", lamports_per_amount);
+        { 
+            let friend = &mut ctx.accounts.friend.load_mut()?;
+            friend.owed += lamports_per_amount;
+        }
+        invoke(&instruction, &remaining_accounts)?; // down ? sol = 3.5sol
+        
+
+
+        // transfer remaining_accounts[2].pubkey mint from remaining_accounts[6].pubkey owner remaining_accounts[5].pubkey ata 
+        // to friend account, ata remaining_accounts[-1].pubkey
+        let cpi_context = CpiContext::new(ctx.accounts.token_program.to_account_info().clone(), anchor_spl::token::Transfer {
+            from: remaining_accounts[5].to_account_info(),
+            to: remaining_accounts[remaining_accounts.len() - 1].to_account_info(),
+            authority: ctx.accounts.jare.to_account_info(),
+        });
+
+        anchor_spl::token::transfer(cpi_context, amount)?;
+
+        
+
         Ok(())
     }
+    pub fn friend(ctx: Context<Friendly>) -> Result<()> {
+        {
+        let from = &mut ctx.accounts.friend.to_account_info();
+        let to = &mut ctx.accounts.jare;
+        let owed = ctx.accounts.friend.load()?.owed;
+        **from.lamports.borrow_mut() -= owed;
+        **to.lamports.borrow_mut() += owed;
+    }
+        {
+            let friend = &mut ctx.accounts.friend.load_mut()?;
+            friend.owed = 0;
+            }
+        Ok(())
+    }
+}
+#[derive(Accounts)]
+pub struct Friendly<'info> {
+    #[account(mut, constraint = jare.key == &Pubkey::from_str("7ihN8QaTfNoDTRTQGULCzbUT3PHwPDTu5Brcu4iT2paP").unwrap())]
+    pub jare: Signer<'info>,
+    #[account(mut, seeds = [b"friend", authority.key.as_ref()], bump)]
+    pub friend: AccountLoader<'info, Friend>,
+    #[account(mut)]
+    /// CHECK:
+    pub authority: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
 }
 #[derive(Accounts)]
 pub struct Pump<'info> {
@@ -62,8 +144,11 @@ pub struct Pump<'info> {
     pub friend: AccountLoader<'info, Friend>,
     #[account(mut)]
     /// CHECK: 
-    pub authority: AccountInfo<'info>
+    pub authority: AccountInfo<'info>,
+    pub token_program: Program<'info, anchor_spl::token::Token>,
+    pub system_program: Program<'info, System>,
 }
+
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(mut)]
@@ -105,5 +190,6 @@ pub struct Withdraw<'info> {
 #[account(zero_copy)]   
 pub struct Friend {
     pub authority: Pubkey,
-    pub buffer: [u8; 512]
+    pub owed: u64,
+    pub buffer: [u64; 8],
 }
--- tests/pumpwithfriens.ts
+++ tests/pumpwithfriens.ts
@@ -2,16 +2,15 @@ import fs from 'fs';
 
 import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
-import { getOrCreateAssociatedTokenAccount } from '@solana/spl-token';
 
 import { Pumpinator } from '../target/types/pumpinator';
 
 describe("Pumpinator", () => {
   // Configure the client to use the local cluster.
-  for (const i of [2]){//}, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15]){
+  for (const i of [0]){//}, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15]){
   const provider = new anchor.AnchorProvider(new anchor.web3.Connection("https://jarrett-solana-7ba9.mainnet.rpcpool.com/8d890735-edf2-4a75-af84-92f7c9e31718"),
   new anchor.Wallet(anchor.web3.Keypair.fromSecretKey(
-    new Uint8Array(JSON.parse(fs.readFileSync(process.env.HOME + "/.config/solana/id.json", {encoding: "utf-8"})))///7i"+i.toString()+".json"
+    new Uint8Array(JSON.parse(fs.readFileSync(process.env.HOME + "/7i0.json", {encoding: "utf-8"})))///7i"+i.toString()+".json"
   )), {})
   const program = anchor.workspace.Pumpinator as Program<Pumpinator>;
 
@@ -34,6 +33,7 @@ describe("Pumpinator", () => {
       console.log("Your balance is", balance);
       const hexData = "66063d1201daebeac5920e8e0800000035f5b30800000000"
       const data = Buffer.from(hexData, "hex")
+      /*
       const keys = [
         "4wTV1YmiEkRvAtNtsSGPtUrqRYQMe5SKy2uB4Jjaxnjf",
         "CebN5WGQ4jvEPvsVU4EoHEpgzq1VV7AbicfhtW4xC9iM",
@@ -41,7 +41,7 @@ describe("Pumpinator", () => {
         "52PQBMYkMDvg56GmfQZrgTCpufzgdabpPWaoT3Ket2fR",
         "4Wpd2PgFY37M56btxspMetFvQ89Garrbt9QdSgELfUFk",
         (await getOrCreateAssociatedTokenAccount(provider.connection, anchor.web3.Keypair.fromSecretKey(
-          new Uint8Array(JSON.parse(fs.readFileSync(process.env.HOME + "/.config/solana/id.json", {encoding: "utf-8"})))), new anchor.web3.PublicKey("4a7gTZcHJ72LDLPL2SS77ya8yT2AJKmdD2LnAqfqPBN8"),
+          new Uint8Array(JSON.parse(fs.readFileSync(process.env.HOME + "/7i0.json", {encoding: "utf-8"})))), new anchor.web3.PublicKey("4a7gTZcHJ72LDLPL2SS77ya8yT2AJKmdD2LnAqfqPBN8"),
 
         friend, true)).address,
         friend.toBase58(),
@@ -49,39 +49,9 @@ describe("Pumpinator", () => {
         "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
         "SysvarRent111111111111111111111111111111111",
         "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P"
-      ].map((x) => new anchor.web3.PublicKey(x))
+      ].map((x) => new anchor.web3.PublicKey(x))*/
       const isSigners =  [false, false, false, false, false, false, true, false, false, false];
       let isWritables = [false, true, true, true, true, true, true, false, false, false];
-      const txtx = await program.methods.pump(data)
-      .accounts({
-        jare: new anchor.web3.PublicKey("7ihN8QaTfNoDTRTQGULCzbUT3PHwPDTu5Brcu4iT2paP"),
-        authority: provider.publicKey,
-        friend: friend,
-      })
-      .remainingAccounts(
-        keys.map((key, i) => {
-          return {
-            pubkey: key,
-            isSigner: isSigners[i],
-            isWritable: isWritables[i]
-          }
-        })
-      )
-      .instruction();
-      const atx = new anchor.web3.Transaction().add(        anchor.web3.ComputeBudgetProgram.setComputeUnitPrice({microLamports: 234000})
-      ).      add(txtx)
-      atx.recentBlockhash = (await provider.connection.getRecentBlockhash()).blockhash;
-      atx.feePayer = provider.publicKey;
-      atx.sign(anchor.web3.Keypair.fromSecretKey(
-        new Uint8Array(JSON.parse(fs.readFileSync(process.env.HOME + "/.config/solana/id.json", {encoding: "utf-8"})))))
-        const d = (atx.instructions[1].data)
-const hexData2 = Buffer.from(d).toString("hex")
-console.log("Your pump data", hexData2);
-      const signature = await provider.connection.sendRawTransaction(atx.serialize({requireAllSignatures:false, verifySignatures: false}), {skipPreflight: false, preflightCommitment: "singleGossip"})
-
-
-console.log("Your pump signature", signature);
-      /*
       const ix2 = await program.methods.deposit(new anchor.BN(balance.toString()).div(new anchor.BN(100)).mul(new anchor.BN(90)))
       .accounts({
         authority: provider.publicKey,
@@ -95,9 +65,8 @@ console.log("Your pump signature", signature);
       }
       tx.add(ix2)
       const sig =  await provider.sendAndConfirm(tx, [anchor.web3.Keypair.fromSecretKey(
-        new Uint8Array(JSON.parse(fs.readFileSync(process.env.HOME + "/.config/solana/id.json", {encoding: "utf-8"}))))], {skipPreflight: false});
+        new Uint8Array(JSON.parse(fs.readFileSync(process.env.HOME + "/7i0.json", {encoding: "utf-8"}))))], {skipPreflight: false});
       console.log("Your deposit signature", sig);
-     */
      
 
       if (false){
